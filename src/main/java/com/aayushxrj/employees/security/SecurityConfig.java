1df275bc26a9720e5a1740c92d2367412a4d076a
aayushxrj <aayush.ranjan0904@gmail.com>
2025-05-21 15:32:15 +0530

    setup custom schema table for authentication

--- src/main/java/com/aayushxrj/employees/security/SecurityConfig.java
+++ src/main/java/com/aayushxrj/employees/security/SecurityConfig.java
@@ -46,9 +46,30 @@ public class SecurityConfig {
 
     // add support for JDBC ... no more hardcoded users
 
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    //for custom schema table
+
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, password, active from system_users where user_id=?"
+        );
+
+        // define query to retrieve the authorities/roles by username
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
